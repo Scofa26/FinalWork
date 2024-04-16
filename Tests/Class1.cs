52588f2fe6d7fe23484cc00a044e561eec0adf40
Sophia <chida2001@mail.ru>
2024-04-16 22:55:24 +0300

    test

---
+++ Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalWork.Tests
+{
+    internal class Class1
+    {
+    }
+}
